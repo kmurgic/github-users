bbb9b3e283b8d0b61e04554f12221d99bd4104f2
Keith Murgic <keith.murgic@brandpulse.io>
2020-07-03 15:30:12 -0500

    Updates

---
+++ precache-manifest.2a1d8d85ffc003eb61566375e81cc659.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c3d3d2d5adacf4f3eb3589250024fa1",
+    "url": "/github-users/index.html"
+  },
+  {
+    "revision": "f86776af09fd7fc2cb36",
+    "url": "/github-users/static/css/main.fe03f47d.chunk.css"
+  },
+  {
+    "revision": "4bf174da37bf10d00282",
+    "url": "/github-users/static/js/2.a98fb8b7.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/github-users/static/js/2.a98fb8b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f86776af09fd7fc2cb36",
+    "url": "/github-users/static/js/main.7e6c8b5a.chunk.js"
+  },
+  {
+    "revision": "f453baa956893dd8728d",
+    "url": "/github-users/static/js/runtime-main.75e0f370.js"
+  }
+]);
